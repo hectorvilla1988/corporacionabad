4c64502b9da83ef07549eb39f242b1fb22a19b87
Hector Villa <pphvilla@gmail.com>
2020-02-23 20:20:04 -0500

    ultimos cambios

--- procesar.php
+++ procesar.php
@@ -4,7 +4,7 @@ $nombre = $_POST["nombre"];
 $correo = $_POST["correo"];
 $telefono = $_POST["telefono"];
 $mensaje = $_POST["mensaje"];
-$contenido = "Nombne: ".$nombre. "\nCorreo:" .$correo. "\nTeléfono:" .$telefono. "\nMensaje:" .$mensaje;
+$contenido = "Nombre: ".$nombre. "\nCorreo:" .$correo. "\nTeléfono:" .$telefono. "\nMensaje:" .$mensaje;
 mail($destino,"Contacto", $contenido);
 header("Location:gracias.html");
 
